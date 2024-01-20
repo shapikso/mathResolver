9f935484d2f7c3ce37342a8d4710eb95eb9a70d8
shapovalov.kyrylo.cr <shapovalov.kyrylo.cr@gmail.com>
2024-01-20 16:32:41 +0200

    added ability to see beautiful formula of what you write at input

--- src/components/Integral/Integrate.tsx
+++ src/components/Integral/Integrate.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useState, useEffect} from 'react';
 import './Derivative.scss';
 import Input from "../common/Input/Input";
 import {buttonSize, inputSize} from "../types/types";
@@ -19,12 +19,25 @@ type TProps = {
 
 const Integrate = ({mainExpression}: TProps) => {
     const [result, setResult] = useState('2sqrt(x)');
+    const [mathJaxResult, setMathJaxResult] = useState('2sqrt(x)');
+
     const mainTest = '1/sqrt(x)';
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => setResult(e.target.value);
     const handleClick = () => {
         const isResult = isResultRight(mainExpression.main, result,1,2);
         toast.success(isResult ? 'ваш ответ правильный' : 'ваш ответ не правильный');
     };
+
+    useEffect(() => {
+        try {
+            const parsedResult = math.parse(result).toTex({parenthesis: 'auto'});
+            setMathJaxResult(parsedResult);
+        } catch (error) {
+            // @ts-ignore
+            toast.error(error.message);
+        }
+
+    },[result]);
     //const result = '2x + cos(x)';
     //const mainExpression = generateExpression();
     return (
@@ -32,7 +45,7 @@ const Integrate = ({mainExpression}: TProps) => {
             <div className="derivativeWrapper__derivativeExpression"><MathComponent tex={String.raw`${mainExpression.mathjax}`} /></div>
             <Input value={result} size={inputSize.large} onChange={handleChange}/>
             <Button size={buttonSize.large} onClick={handleClick}>Submit</Button>
-            <div className="derivativeWrapper__derivativeExpression"><MathComponent tex={String.raw`${math.derivative(mainExpression.main, 'x') }`} /></div>
+            <div className="derivativeWrapper__derivativeExpression"><MathComponent tex={mathJaxResult} /></div>
         </div>
     );
 };
