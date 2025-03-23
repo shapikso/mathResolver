3609c34ee24a6efe0fe4f574947f4b6338b78456
shapikso <shapovalov.kyrylo.cr@gmail.com>
2025-03-23 19:41:05 +0200

    added generate integral easy level

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -14,7 +14,8 @@ const SideBar = ({isSideBarShow}:Props) => {
             <div className={'sideBar__nav'}>
                 <Link to={LINKS.BOARD_LINK}><NavItem active={pathname.includes(LINKS.BOARD_LINK)}>Derivative </NavItem></Link>
                 <Link to={LINKS.DERIVATIVE_LINK}><NavItem active={pathname.includes(LINKS.DERIVATIVE_LINK)}>Derivative Generating</NavItem></Link>
-                <Link to={'/'}><NavItem active={pathname.includes(LINKS.INTEGRAL_LINK)}>Integral</NavItem></Link>
+                <Link to={'/'}><NavItem active={pathname === '/'}>Integral</NavItem></Link>
+                <Link to={LINKS.INTEGRAL_LINK}><NavItem active={pathname.includes(LINKS.INTEGRAL_LINK)}>Integral V2</NavItem></Link>
             </div>
         </div>
 
